6324b6f3df0c663ae296b9e9980c982d53940219
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-03-05 10:23:24 +0300

    changed some desktop components

--- src/pages/workoutPage.tsx
+++ src/pages/workoutPage.tsx
@@ -13,7 +13,7 @@ const {background} = useBackgroundimg()
   return (
     <> 
     <div>
-   <div className={part&&(`w-screen  h-52 ${background(part)} bg-center bg-cover object-center relative`)}>
+   <div className={part&&(`w-screen  h-52 md:h-96 ${background(part)} bg-center bg-cover object-center relative`)}>
     <div className="text-3xl text-white bg-black h-[100%] bg-opacity-40 text-center flex items-center justify-center"><span>{part&&(part)}</span></div>
     </div>
     <div>
@@ -26,9 +26,11 @@ const {background} = useBackgroundimg()
     {
      getPart(part)&& (getPart(part).map((item:Exercise)=>{
         return(<>
-        <div className=" flex flex-row-reverse  p-2 my-1 items-center justify-between ">
+        <div className="md:flex md:flex-col md:justify-between md:items-center md:w-screen">
+        <div className=" flex flex-row-reverse  p-2 my-1 items-center justify-between md:w-[75%]">
             <WorkoutDetails workout={item}/>
     <span className="w-5/6 font-bold border-0 border-b-[1px] p-6 border-b-gray-200 ">{item.exercise}</span>
+    </div> 
     </div>
     </>)
       }))
@@ -36,7 +38,7 @@ const {background} = useBackgroundimg()
     <div className="w-screen h-20"></div>
     </section>
 
-   <div className="fixed bottom-0 z-30 border-0 border-t-2 p-2 flex  justify-center items-center w-screen bg-white"> <button className="bg-blue-600 rounded-full p-3 text-center text-white font-bold w-[90%] ">Start</button></div>
+   <div className="fixed md:static  bottom-0 z-30 border-0 border-t-2 p-2 flex  justify-center items-center w-screen bg-white"> <button className="bg-blue-600 rounded-full p-3 text-center text-white font-bold w-[90%] ">Start</button></div>
     </div>
     </div>
     </>
